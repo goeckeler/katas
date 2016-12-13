cfa58d5960d8c3570e5a15c4209a7d9afb505ff8
goeckeler <thorsten@goeckeler.com>
2016-12-13 22:22:46 +0100

    Just formatted and added "final"

--- katas/Gilded Rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ katas/Gilded Rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,20 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class GildedRoseTest {
+public class GildedRoseTest
+{
 
-    @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+  @Test
+  public void foo() {
+    final Item[] items = new Item[] {
+      new Item("foo", 0, 0)
+    };
+    final GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals("fixme", app.items[0].name);
+  }
 
 }
