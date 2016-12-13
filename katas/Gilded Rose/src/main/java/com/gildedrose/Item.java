296ca58686a1120c472525c5f57948e224ea9f11
goeckeler <thorsten@goeckeler.com>
2016-12-13 22:21:15 +0100

    Just applied consistent formatting.

--- katas/Gilded Rose/src/main/java/com/gildedrose/Item.java
+++ katas/Gilded Rose/src/main/java/com/gildedrose/Item.java
@@ -1,21 +1,21 @@
 package com.gildedrose;
 
-public class Item {
+public class Item
+{
+  public String name;
 
-    public String name;
+  public int sellIn;
 
-    public int sellIn;
+  public int quality;
 
-    public int quality;
+  public Item(final String name, final int sellIn, final int quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
 
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
-
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
-    }
+  @Override
+  public String toString() {
+    return this.name + ", " + this.sellIn + ", " + this.quality;
+  }
 }
